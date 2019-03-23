7536b65c10c6de95f31365a28628777c0448fc65
Rafiul Islam <avoidcloud@gmail.com>
2019-03-23 16:23:14 +0600

    model blue print

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+_CATEGORIES = (
+	('Subject', 'Subject'),
+	('Part', 'Part'),
+	('Chapter', 'Chapter'),
+	('Topic', 'Topic'),
+	('Module', 'Module'),
+	('Label', 'Label')
+)
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=10, primary_key=True, choices=_CATEGORIES)
+
+
+class Path(models.Model):
+	location = models.CharField(max_length=32, primary_key=True)
+
+
+class Folder(models.Model):
+	name = models.ForeignKey(Category, on_delete=models.CASCADE)
+	node = models.ForeignKey(Path, on_delete=models.CASCADE, primary_key=True)
+	root = models.ForeignKey(Path, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Post(models.Model):
+	uid = models.CharField(max_length=32, primary_key=True)
+	title = models.TextField()
+	body = models.TextField()
+
+
+class MCQ(models.Model):
+	uid = models.CharField(max_length=32, primary_key=True)
+	question = models.TextField()
+	option1 = models.TextField()
+	option2 = models.TextField()
+	option3 = models.TextField()
+	option4 = models.TextField()
+	answer = models.SmallIntegerField(default=1, 
+		validators=[MaxValueValidator(4), MinValueValidator(1)])
+	folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+
+
+class CQ(models.Model):
+	uid = models.CharField(max_length=32, primary_key=True)
+	question = models.TextField()
+	folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
