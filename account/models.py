e130bb07dd329275749da9623e1cc90d92ac239a
Rafiul <avoidcloud@gmail.com>
2019-09-26 14:27:26 +0600

    model migrate

--- account/models.py
+++ account/models.py
@@ -108,4 +108,3 @@ class Profile(models.Model):
 	institute = models.CharField(max_length=250)
 	bar = models.CharField(max_length=250)
 	session = models.PositiveIntegerField(default=0)
-	meta = JSONField(default=dict)
